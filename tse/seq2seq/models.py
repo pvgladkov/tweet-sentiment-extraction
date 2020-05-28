50dd434d7c27ed81aa04293b5dd9a00c35612a95
Pavel Gladkov <gladkov.p@gmail.com>
2020-05-28 19:29:35 +0300

    seq2seq

--- tse/seq2seq/models.py
+++ tse/seq2seq/models.py
@@ -192,3 +192,37 @@ def tensors_from_pair(pair, input_lang, output_lang):
     return input_tensor, target_tensor
 
 
+def evaluate(encoder, decoder, sentence, input_lang, output_lang, max_length=128):
+    with torch.no_grad():
+        input_tensor = tensor_from_sentence(input_lang, sentence)
+        input_length = input_tensor.size()[0]
+        encoder_hidden = encoder.init_hidden()
+
+        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device())
+
+        for ei in range(input_length):
+            encoder_output, encoder_hidden = encoder(input_tensor[ei],
+                                                     encoder_hidden)
+            encoder_outputs[ei] += encoder_output[0, 0]
+
+        decoder_input = torch.tensor([[SOS_TOKEN]], device=device())  # SOS
+
+        decoder_hidden = encoder_hidden
+
+        decoded_words = []
+        decoder_attentions = torch.zeros(max_length, max_length)
+
+        for di in range(max_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs)
+            decoder_attentions[di] = decoder_attention.data
+            topv, topi = decoder_output.data.topk(1)
+            if topi.item() == EOS_TOKEN:
+                # decoded_words.append('<EOS>')
+                break
+            else:
+                decoded_words.append(output_lang.index2word[topi.item()])
+
+            decoder_input = topi.squeeze().detach()
+
+        return decoded_words, decoder_attentions[:di + 1]
+
