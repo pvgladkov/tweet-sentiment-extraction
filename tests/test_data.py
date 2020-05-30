fb41e9514c447dfed1bfdc7115f3aeb14830c718
Pavel Gladkov <gladkov.p@gmail.com>
2020-05-30 12:03:13 +0300

    tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,16 +3,17 @@ from tse.data import get_indexes, get_char_targets
 
 
 @pytest.mark.parametrize('tweet, selected_text, result', [
-    (' i am feeling good', ' good', (13, 16)),
-    (' lol', ' lol', (0, 2)),
-    (' lol lol', ' lol', (0, 2)),
+    (' i am feeling good', ' good', (14, 17)),
+    (' lol', ' lol', (1, 3)),
+    (' lol lol', ' lol', (1, 3)),
 ])
 def test_get_indexes(tweet, selected_text, result):
     assert get_indexes(tweet, selected_text) == result
 
 
 @pytest.mark.parametrize('tweet, idx0, idx1, result', [
-    ('lol', 0, 2, [1, 1, 1])
+    ('lol', 0, 2, [1, 1, 1]),
+    (' lol', 1, 3, [0, 1, 1, 1]),
 ])
 def test_get_char_targets(tweet, idx0, idx1, result):
     assert get_char_targets(tweet, idx0, idx1) == result
