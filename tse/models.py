084188847f44a9358bed6abe3c9843097b1fda37
Pavel Gladkov <gladkov.p@gmail.com>
2020-05-30 10:04:45 +0300

    model

--- tse/models.py
+++ tse/models.py
@@ -8,7 +8,7 @@ class TweetModel(transformers.BertPreTrainedModel):
         super(TweetModel, self).__init__(conf)
         self.bert = transformers.RobertaModel.from_pretrained(train_config.BERT_PATH, config=conf)
         self.drop_out = nn.Dropout(0.1)
-        self.l0 = nn.Linear(768 * 2, 2)
+        self.l0 = nn.Linear(768, 2)
         torch.nn.init.normal_(self.l0.weight, std=0.02)
 
     def forward(self, ids, mask, token_type_ids):
@@ -18,7 +18,7 @@ class TweetModel(transformers.BertPreTrainedModel):
             token_type_ids=token_type_ids
         )
 
-        out = torch.cat((out[-1], out[-2]), dim=-1)
+        out = 0.3*out[-1] + 0.2*out[-2] + 0.1*out[-3] + 0.1*out[-4] + 0.1*out[-5] + 0.1*out[-6] + 0.1*out[-7]
         out = self.drop_out(out)
         logits = self.l0(out)
 
