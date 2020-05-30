93624e93eb3cc022f142b76278215106b3b214ee
Pavel Gladkov <gladkov.p@gmail.com>
2020-05-30 10:05:57 +0300

    roberta

--- tse_run.py
+++ tse_run.py
@@ -1,23 +1,27 @@
-import torch
-import pandas as pd
-import torch.nn as nn
-import numpy as np
+from functools import reduce
 
-import transformers
+import numpy as np
+import pandas as pd
 import tokenizers
+import torch
+import torch.nn as nn
+import transformers
+from tqdm.autonotebook import tqdm
 from transformers import AdamW
 from transformers import get_linear_schedule_with_warmup
-from tqdm.autonotebook import tqdm
+
 from tse.data import TweetDataset
 from tse.models import TweetModel
-from tse.utils import create_folds, jaccard, AverageMeter, bert_output_to_string, EarlyStopping
+from tse.utils import create_folds, jaccard, AverageMeter, bert_output_to_string, EarlyStopping, set_seed, device
+from torch.utils.data import DataLoader
+
 
 # class KaggleTrainConfig:
 #     MAX_LEN = 192
 #     TRAIN_BATCH_SIZE = 32
 #     VALID_BATCH_SIZE = 8
 #     EPOCHS = 5
-#     BERT_PATH = "../input/bert-base-uncased/"
+#     BERT_PATH = "../input/roberta-base/"
 #     MODEL_PATH = "model.bin"
 #     TRAINING_FILE = "train_folds.csv"
 #     TEST_FILE = "../input/tweet-sentiment-extraction/test.csv"
@@ -61,59 +65,60 @@ def loss_fn(start_logits, end_logits, start_positions, end_positions):
     return total_loss
 
 
-def _batch_jaccard(outputs_start, outputs_end, batch):
+def _batch_jaccard(outputs_start_, outputs_end_, batch):
     orig_tweet = batch['orig_tweet']
     orig_selected = batch['orig_selected']
     sentiment = batch['sentiment']
     offsets = batch['offsets']
 
-    outputs_start = torch.softmax(outputs_start, dim=1).cpu().detach().numpy()
-    outputs_end = torch.softmax(outputs_end, dim=1).cpu().detach().numpy()
+    outputs_start_ = torch.softmax(outputs_start_, dim=1).cpu().detach().numpy()
+    outputs_end_ = torch.softmax(outputs_end_, dim=1).cpu().detach().numpy()
     jaccard_scores = []
-    filtered_outputs = []
+    filtered_outputs_ = []
     for px, tweet in enumerate(orig_tweet):
         selected_tweet = orig_selected[px]
         tweet_sentiment = sentiment[px]
-        f_output = bert_output_to_string(tweet, tweet_sentiment, np.argmax(outputs_start[px, :]),
-                                         np.argmax(outputs_end[px, :]), offsets[px])
+        f_output = bert_output_to_string(tweet, tweet_sentiment, np.argmax(outputs_start_[px, :]),
+                                         np.argmax(outputs_end_[px, :]), offsets[px])
 
         jaccard_score = jaccard(selected_tweet.strip(), f_output.strip())
         jaccard_scores.append(jaccard_score)
-        filtered_outputs.append(f_output)
-    return jaccard_scores, filtered_outputs
+        filtered_outputs_.append(f_output)
+    return jaccard_scores, filtered_outputs_
 
 
-def train_fn(data_loader, model, optimizer, device, scheduler):
-    model.train()
+def train_fn(data_loader_, model_, optimizer, scheduler):
+    model_.train()
     losses = AverageMeter()
     jaccards = AverageMeter()
 
-    tk0 = tqdm(data_loader, total=len(data_loader))
+    tk0 = tqdm(data_loader_, total=len(data_loader_))
+
+    device_ = device()
 
     for bi, d in enumerate(tk0):
+        ids = d["ids"].to(device_, dtype=torch.long)
+        token_type_ids = d["token_type_ids"].to(device_, dtype=torch.long)
+        mask = d["mask"].to(device_, dtype=torch.long)
+        targets_start = d["targets_start"].to(device_, dtype=torch.long)
+        targets_end = d["targets_end"].to(device_, dtype=torch.long)
 
-        ids = d["ids"].to(device, dtype=torch.long)
-        token_type_ids = d["token_type_ids"].to(device, dtype=torch.long)
-        mask = d["mask"].to(device, dtype=torch.long)
-        targets_start = d["targets_start"].to(device, dtype=torch.long)
-        targets_end = d["targets_end"].to(device, dtype=torch.long)
-
-        model.zero_grad()
-        outputs_start, outputs_end = model(ids=ids, mask=mask, token_type_ids=token_type_ids)
-        loss = loss_fn(outputs_start, outputs_end, targets_start, targets_end)
+        model_.zero_grad()
+        outputs_start_, outputs_end_ = model_(ids=ids, mask=mask, token_type_ids=token_type_ids)
+        loss = loss_fn(outputs_start_, outputs_end_, targets_start, targets_end)
 
         loss.backward()
         optimizer.step()
         scheduler.step()
 
-        jaccard_scores, _ = _batch_jaccard(outputs_start, outputs_end, d)
+        jaccard_scores, _ = _batch_jaccard(outputs_start_, outputs_end_, d)
 
         jaccards.update(np.mean(jaccard_scores), ids.size(0))
         losses.update(loss.item(), ids.size(0))
         tk0.set_postfix(loss=losses.avg, jaccard=jaccards.avg)
 
 
-def eval_fn(data_loader, model, device, epoch, fold):
+def eval_fn(data_loader, model, epoch, fold):
     model.eval()
     losses = AverageMeter()
     jaccards = AverageMeter()
@@ -122,33 +127,35 @@ def eval_fn(data_loader, model, device, epoch, fold):
     target_texts = []
     predicted_texts = []
     sentiments = []
-    scores = []
+    scores_ = []
+
+    device_ = device()
 
     with torch.no_grad():
         tk0 = tqdm(data_loader, total=len(data_loader))
         for bi, d in enumerate(tk0):
-            ids = d["ids"].to(device, dtype=torch.long)
-            token_type_ids = d["token_type_ids"].to(device, dtype=torch.long)
-            mask = d["mask"].to(device, dtype=torch.long)
-            targets_start = d["targets_start"].to(device, dtype=torch.long)
-            targets_end = d["targets_end"].to(device, dtype=torch.long)
+            ids = d["ids"].to(device_, dtype=torch.long)
+            token_type_ids = d["token_type_ids"].to(device_, dtype=torch.long)
+            mask = d["mask"].to(device_, dtype=torch.long)
+            targets_start = d["targets_start"].to(device_, dtype=torch.long)
+            targets_end = d["targets_end"].to(device_, dtype=torch.long)
 
-            outputs_start, outputs_end = model(ids=ids, mask=mask, token_type_ids=token_type_ids)
-            loss = loss_fn(outputs_start, outputs_end, targets_start, targets_end)
+            outputs_start_, outputs_end_ = model(ids=ids, mask=mask, token_type_ids=token_type_ids)
+            loss = loss_fn(outputs_start_, outputs_end_, targets_start, targets_end)
 
-            jaccard_scores, filtered_outputs = _batch_jaccard(outputs_start, outputs_end, d)
+            jaccard_scores, filtered_outputs = _batch_jaccard(outputs_start_, outputs_end_, d)
             jaccards.update(np.mean(jaccard_scores), ids.size(0))
             losses.update(loss.item(), ids.size(0))
             tk0.set_postfix(loss=losses.avg, jaccard=jaccards.avg)
 
             predicted_texts += filtered_outputs
-            scores += jaccard_scores
+            scores_ += jaccard_scores
             tweets += d['orig_tweet']
             target_texts += d['orig_selected']
             sentiments += d['sentiment']
 
     df = pd.DataFrame({'tweet': tweets, 'target_text': target_texts,
-                       'predicted_text': predicted_texts, 'sentiment': sentiments, 'scores': scores})
+                       'predicted_text': predicted_texts, 'sentiment': sentiments, 'scores': scores_})
     file_name = f'debug_{epoch}_{fold}.csv'
     df.to_csv(file_name, index=False, encoding='utf-8')
     print(f'save {file_name}')
@@ -168,7 +175,7 @@ def run(fold):
         selected_text=df_train.selected_text.values
     )
 
-    train_data_loader = torch.utils.data.DataLoader(
+    train_data_loader = DataLoader(
         train_dataset,
         batch_size=train_config.TRAIN_BATCH_SIZE,
         num_workers=4
@@ -181,17 +188,16 @@ def run(fold):
         selected_text=df_valid.selected_text.values
     )
 
-    valid_data_loader = torch.utils.data.DataLoader(
+    valid_data_loader = DataLoader(
         valid_dataset,
         batch_size=train_config.VALID_BATCH_SIZE,
         num_workers=2
     )
 
-    device = torch.device("cuda")
     model_config = transformers.BertConfig.from_pretrained(train_config.BERT_PATH)
     model_config.output_hidden_states = True
     model = TweetModel(train_config, conf=model_config)
-    model.to(device)
+    model.to(device())
 
     num_train_steps = int(len(df_train) / train_config.TRAIN_BATCH_SIZE * train_config.EPOCHS)
     param_optimizer = list(model.named_parameters())
@@ -200,6 +206,7 @@ def run(fold):
         {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.001},
         {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0},
     ]
+
     optimizer = AdamW(optimizer_parameters, lr=3e-5)
     scheduler = get_linear_schedule_with_warmup(
         optimizer,
@@ -207,70 +214,50 @@ def run(fold):
         num_training_steps=num_train_steps
     )
 
-    es = EarlyStopping(patience=2, mode="max")
+    es = EarlyStopping(patience=2, mode="max", delta=0.001)
     print(f"Training is Starting for fold={fold}")
 
-    # I'm training only for 3 epochs even though I specified 5!!!
+    jaccard_ = 0
     for epoch in range(train_config.EPOCHS):
-        train_fn(train_data_loader, model, optimizer, device, scheduler=scheduler)
-        jaccard = eval_fn(valid_data_loader, model, device, epoch, fold)
-        print(f"Jaccard Score = {jaccard}")
-        es(jaccard, model, model_path=f"model_{fold}.bin")
+        train_fn(train_data_loader, model, optimizer, scheduler=scheduler)
+        jaccard_ = eval_fn(valid_data_loader, model, epoch, fold)
+        print(f"Jaccard Score = {jaccard_}")
+        es(jaccard_, model, model_path=f"model_{fold}.bin")
         if es.early_stop:
             print("Early stopping")
             break
+    return jaccard_
 
 
 if __name__ == '__main__':
 
+    set_seed(14)
+
     create_folds(train_config)
 
-    print('fold 0')
-    run(fold=0)
-
-    print('fold 1')
-    run(fold=1)
-
-    print('fold 2')
-    run(fold=2)
-
-    print('fold 3')
-    run(fold=3)
-
-    print('fold 4')
-    run(fold=4)
-
-    df_test = pd.read_csv(train_config.TEST_FILE)
-    df_test.loc[:, "selected_text"] = df_test.text.values
-
-    device = torch.device("cuda")
     model_config = transformers.BertConfig.from_pretrained(train_config.BERT_PATH)
     model_config.output_hidden_states = True
 
-    model1 = TweetModel(train_config, conf=model_config)
-    model1.to(device)
-    model1.load_state_dict(torch.load("model_0.bin"))
-    model1.eval()
+    scores = []
+    for fold in range(5):
+        print(f'fold {fold}')
+        j = run(fold=fold)
+        scores.append((fold, j))
 
-    model2 = TweetModel(train_config, conf=model_config)
-    model2.to(device)
-    model2.load_state_dict(torch.load("model_1.bin"))
-    model2.eval()
+    sorted_scores = sorted(scores, key=lambda x: -x[1])
+    print(sorted_scores)
 
-    model3 = TweetModel(train_config, conf=model_config)
-    model3.to(device)
-    model3.load_state_dict(torch.load("model_2.bin"))
-    model3.eval()
+    models = []
+    for fold, _ in sorted_scores[:5]:
+        print(f"loading model_{fold}.bin")
+        model = TweetModel(train_config, conf=model_config)
+        model.to(device())
+        model.load_state_dict(torch.load(f"model_{fold}.bin"))
+        model.eval()
+        models.append(model)
 
-    model4 = TweetModel(train_config, conf=model_config)
-    model4.to(device)
-    model4.load_state_dict(torch.load("model_3.bin"))
-    model4.eval()
-
-    model5 = TweetModel(train_config, conf=model_config)
-    model5.to(device)
-    model5.load_state_dict(torch.load("model_4.bin"))
-    model5.eval()
+    df_test = pd.read_csv(train_config.TEST_FILE)
+    df_test.loc[:, "selected_text"] = df_test.text.values
 
     final_output = []
 
@@ -291,18 +278,20 @@ if __name__ == '__main__':
     with torch.no_grad():
         tk0 = tqdm(data_loader, total=len(data_loader))
         for bi, d in enumerate(tk0):
-            ids = d["ids"].to(device, dtype=torch.long)
-            token_type_ids = d["token_type_ids"].to(device, dtype=torch.long)
-            mask = d["mask"].to(device, dtype=torch.long)
+            ids = d["ids"].to(device(), dtype=torch.long)
+            token_type_ids = d["token_type_ids"].to(device(), dtype=torch.long)
+            mask = d["mask"].to(device(), dtype=torch.long)
 
-            outputs_start1, outputs_end1 = model1(ids=ids, mask=mask, token_type_ids=token_type_ids)
-            outputs_start2, outputs_end2 = model2(ids=ids, mask=mask, token_type_ids=token_type_ids)
-            outputs_start3, outputs_end3 = model3(ids=ids, mask=mask, token_type_ids=token_type_ids)
-            outputs_start4, outputs_end4 = model4(ids=ids, mask=mask, token_type_ids=token_type_ids)
-            outputs_start5, outputs_end5 = model5(ids=ids, mask=mask, token_type_ids=token_type_ids)
+            starts_list = []
+            ends_list = []
 
-            outputs_start = (outputs_start1 + outputs_start2 + outputs_start3 + outputs_start4 + outputs_start5) / 5
-            outputs_end = (outputs_end1 + outputs_end2 + outputs_end3 + outputs_end4 + outputs_end5) / 5
+            for model in models:
+                outputs_start, outputs_end = model(ids=ids, mask=mask, token_type_ids=token_type_ids)
+                starts_list.append(outputs_start)
+                ends_list.append(outputs_end)
+
+            outputs_start = (starts_list[0] + starts_list[1] + starts_list[2] + starts_list[3] + starts_list[4]) / len(starts_list)
+            outputs_end = (ends_list[0] + ends_list[1] + ends_list[2] + ends_list[3] + ends_list[4]) / len(ends_list)
 
             _, filtered_outputs = _batch_jaccard(outputs_start, outputs_end, d)
             for s in filtered_outputs:
